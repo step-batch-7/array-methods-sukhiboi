c4c45312acef9a1080e3b870f647d6bfa0e10cbc
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-05-08 20:45:03 +0530

    refactored filter function

--- filter.c
+++ filter.c
@@ -4,13 +4,8 @@ Array_ptr filter(Array_ptr src, Predicate predicate)
 {
     int temp[src->length], count = 0;
     for (int i = 0; i < src->length; i++)
-    {
         if ((*predicate)(src->array[i]))
-        {
-            temp[count] = src->array[i];
-            count++;
-        }
-    }
+            temp[count++] = src->array[i];
     Array_ptr filtered_array = create_array(count);
     for (int i = 0; i < count; i++)
         filtered_array->array[i] = temp[i];
