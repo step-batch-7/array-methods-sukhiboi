9e4e6bacfd0afe74ea13b1ab014503b67ad95e81
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-05-08 20:10:42 +0530

    created reduce.c

--- numbers.c
+++ numbers.c
@@ -1,4 +1,13 @@
+#include <stdio.h>
 #include "array.h"
+
+int square(int value);
+Bool is_deivisible_by_two(int value);
+int sum(int num_1, int num_2);
+
+void run_map();
+void run_filter();
+void run_reduce();
 
 int square(int value)
 {
@@ -7,34 +16,51 @@ int square(int value)
 
 Bool is_deivisible_by_two(int value)
 {
-    if (value % 2  == 0)
+    if (value % 2 == 0)
         return True;
     return False;
 }
 
-void run_map()
+int sum(int num_1, int num_2)
+{
+    return num_1 + num_2;
+}
+
+Array *create_simple_array(void)
 {
     Array *src = create_array(4);
     src->array[0] = 1;
     src->array[1] = 10;
     src->array[2] = 12;
     src->array[3] = 19;
+    return src;
+}
+
+void run_map()
+{
+    Array *src = create_simple_array();
     display(map(src, &square));
 }
 
 void run_filter()
 {
-    Array *src = create_array(4);
-    src->array[0] = 1;
-    src->array[1] = 10;
-    src->array[2] = 12;
-    src->array[3] = 19;
+    Array *src = create_simple_array();
     display(filter(src, &is_deivisible_by_two));
+}
+
+void run_reduce()
+{
+    Array *src = create_simple_array();
+    printf("result: %d", reduce(src, 0, &sum));
 }
 
 int main(void)
 {
+    printf("\nRunning map\n");
     run_map();
+    printf("\nRunning filter\n");
     run_filter();
+    printf("\nRunning reduce\n");
+    run_reduce();
     return 0;
 }
---
+++ reduce.c
@@ -0,0 +1,9 @@
+#include "array.h"
+
+int reduce(Array *src, int init, Reducer reducer)
+{
+    int result = init;
+    for (int i = 0; i < src->length; i++)
+        result = reducer(result, src->array[i]);
+    return result;
+}
