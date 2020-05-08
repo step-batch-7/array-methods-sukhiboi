2c3a6e98cb92c8c234a6ccd50afc2d4c9ba3399f
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-05-08 20:11:34 +0530

    refactored filter function

--- filter.c
+++ filter.c
@@ -4,13 +4,13 @@ Array *filter(Array *src, Predicate predicate)
 {
     int length_filtered_array = 0;
     for (int i = 0; i < src->length; i++)
-        if (predicate(src->array[i]))
+        if (predicate(src->array[i]) == True)
         {
             length_filtered_array++;
         }
     Array *filtered_array = create_array(length_filtered_array);
     for (int i = 0, idx = 0; i < src->length; i++)
-        if (predicate(src->array[i]))
+        if (predicate(src->array[i]) == True)
         {
             filtered_array->array[idx] = src->array[i];
             idx++;
