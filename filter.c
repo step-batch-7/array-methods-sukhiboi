1eb10a80dad4fafa714990bba9bbb9b0cc40e26d
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-05-08 20:00:25 +0530

    created filter.c

---
+++ filter.c
@@ -0,0 +1,19 @@
+#include "array.h"
+
+Array *filter(Array *src, Predicate predicate)
+{
+    int length_filtered_array = 0;
+    for (int i = 0; i < src->length; i++)
+        if (predicate(src->array[i]))
+        {
+            length_filtered_array++;
+        }
+    Array *filtered_array = create_array(length_filtered_array);
+    for (int i = 0, idx = 0; i < src->length; i++)
+        if (predicate(src->array[i]))
+        {
+            filtered_array->array[idx] = src->array[i];
+            idx++;
+        }
+    return filtered_array;
+}
--- numbers.c
+++ numbers.c
@@ -5,6 +5,13 @@ int square(int value)
     return value * value;
 }
 
+Bool is_deivisible_by_two(int value)
+{
+    if (value % 2  == 0)
+        return True;
+    return False;
+}
+
 void run_map()
 {
     Array *src = create_array(4);
@@ -15,8 +22,19 @@ void run_map()
     display(map(src, &square));
 }
 
+void run_filter()
+{
+    Array *src = create_array(4);
+    src->array[0] = 1;
+    src->array[1] = 10;
+    src->array[2] = 12;
+    src->array[3] = 19;
+    display(filter(src, &is_deivisible_by_two));
+}
+
 int main(void)
 {
     run_map();
+    run_filter();
     return 0;
 }
